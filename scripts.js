f72fecdcfd93275617e89d7c723084e6e34db10c
Rudapcard <rudapassoscardoso95@gmail.com>
2024-05-24 12:07:18 -0300

    acrescentando mais um input de resultados

--- scripts.js
+++ scripts.js
@@ -3,21 +3,31 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
     const result = Math.floor(Math.random() * (max - min + 1) + min);
+    const resultadoInput = document.getElementById("resultadoInput");
 
     if( min >= max){
         alert("O valor minimo tem que ser MENOR que o valor máximo")
-        } else {
+    } else {
             var resultado = "Seu resultado desejado"; // Aqui você define o resultado desejado
     
             document.getElementById("resultadoInput").value = result;
         }
 
-    
-    
+        
+    if (resultadoInput.style.display === "none") {
+        resultadoInput.style.display = "block";
+    } else {
+        resultadoInput.style.display = "none";
+    }
 
-    
+
+
+
+
 }
 
+
+    
 
 
 
