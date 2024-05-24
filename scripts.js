7307ac1b273574a730baf27132a59cd44d91d231
Rudapcard <rudapassoscardoso95@gmail.com>
2024-05-24 00:53:46 -0300

    Alteração no codigo js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,22 @@ function generateNumber(){
     
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
-
     const result = Math.floor(Math.random() * (max - min + 1) + min);
 
-    alert(result)
+    if( min >= max){
+        alert("O valor minimo tem que ser MENOR que o valor máximo")
+        } else {
+            var resultado = "Seu resultado desejado"; // Aqui você define o resultado desejado
+    
+            document.getElementById("resultadoInput").value = result;
+        }
+
+    
+    
+
+    
 }
+
 
 
 
